0ccdba0c8117f387985177e97e338f427139ab7b
Edward Mendoza <lalo.garibo90@gmail.com>
2020-06-06 19:27:01 -0700

    Installed router package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import LoginContainer from "./components/login-signup/LoginContainer";
+import Register from "./components/login-signup/Register";
+import Login from "./components/login-signup/Login";
 
 function App() {
   return (
-    <div className="App">
-      <LoginContainer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route path="/signup" component={Register} />
+        <Route path="/login" component={Login} />
+      </div>
+    </BrowserRouter>
   );
 }
 
