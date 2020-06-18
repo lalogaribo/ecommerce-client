77bdd61e4b589726c234c639846d2925438d5082
Edward Mendoza <lalo.garibo90@gmail.com>
2020-06-18 12:03:12 -0700

    Updates

---
+++ precache-manifest.96ef07fc7c9e2267c702b6b8c88294f5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15cc075d5701ad2f21e231271fbf85ce",
+    "url": "/lalogaribo/ecommerce-client/index.html"
+  },
+  {
+    "revision": "475d2bc765f5d937f52f",
+    "url": "/lalogaribo/ecommerce-client/static/css/2.6d95d677.chunk.css"
+  },
+  {
+    "revision": "dc355da80a02af468438",
+    "url": "/lalogaribo/ecommerce-client/static/css/main.63964166.chunk.css"
+  },
+  {
+    "revision": "475d2bc765f5d937f52f",
+    "url": "/lalogaribo/ecommerce-client/static/js/2.6f533840.chunk.js"
+  },
+  {
+    "revision": "4a1691fd44116c5f8c99a494e6966d73",
+    "url": "/lalogaribo/ecommerce-client/static/js/2.6f533840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc355da80a02af468438",
+    "url": "/lalogaribo/ecommerce-client/static/js/main.f1135602.chunk.js"
+  },
+  {
+    "revision": "0a867a9c928cdccbd9c7",
+    "url": "/lalogaribo/ecommerce-client/static/js/runtime-main.6dabcaee.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/lalogaribo/ecommerce-client/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/lalogaribo/ecommerce-client/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/lalogaribo/ecommerce-client/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/lalogaribo/ecommerce-client/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/lalogaribo/ecommerce-client/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/lalogaribo/ecommerce-client/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/lalogaribo/ecommerce-client/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/lalogaribo/ecommerce-client/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/lalogaribo/ecommerce-client/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/lalogaribo/ecommerce-client/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/lalogaribo/ecommerce-client/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/lalogaribo/ecommerce-client/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/lalogaribo/ecommerce-client/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/lalogaribo/ecommerce-client/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/lalogaribo/ecommerce-client/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/lalogaribo/ecommerce-client/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
