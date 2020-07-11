f465e6719de209de0d1a0615706de5e35fcd40b4
Edward Mendoza <lalo.garibo90@gmail.com>
2020-07-11 12:22:02 -0700

    Completed refactor of sidebar component

--- src/components/shared/Filters.js
+++ src/components/shared/Filters.js
@@ -1,109 +1,109 @@
-import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
+import React, {useState, useEffect} from "react";
+import {connect} from "react-redux";
 import {
-  getProductsByMinPrice,
-  getProductsByMaxPrice,
-  getProductsByKeyword,
+	getProductsByMinPrice,
+	getProductsByMaxPrice,
+	getProductsByKeyword,
 } from "../../actions/products";
 import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import "./shared.css";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
-  },
+	root: {
+		"& > *": {
+			margin: theme.spacing(1),
+			width: "25ch",
+		},
+	},
 }));
 
 function Filters({
-  getProductsByMaxPrice,
-  getProductsByMinPrice,
-  getProductsByKeyword,
-}) {
-  const [minPrice, setMinPrice] = useState(0);
-  const [maxPrice, setMaxPrice] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const classes = useStyles();
+									 getProductsByMaxPrice,
+									 getProductsByMinPrice,
+									 getProductsByKeyword,
+								 }) {
+	const [minPrice, setMinPrice] = useState(0);
+	const [maxPrice, setMaxPrice] = useState(0);
+	const [keyword, setKeyword] = useState("");
+	const classes = useStyles();
 
-  const handleMinPrice = (e) => {
-    setMinPrice(e.target.value);
-  };
+	const handleMinPrice = (e) => {
+		setMinPrice(e.target.value);
+	};
 
-  const handleMaxPrice = (e) => {
-    setMaxPrice(e.target.value);
-  };
+	const handleMaxPrice = (e) => {
+		setMaxPrice(e.target.value);
+	};
 
-  const handleKeyword = (e) => {
-    setKeyword(e.target.value);
-  };
+	const handleKeyword = (e) => {
+		setKeyword(e.target.value);
+	};
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    getProductsByKeyword(keyword);
-  };
+	const onSubmit = (e) => {
+		e.preventDefault();
+		getProductsByKeyword(keyword);
+	};
 
-  const onMinSubmit = (e) => {
-    e.preventDefault();
-    getProductsByMinPrice(minPrice);
-  };
+	const onMinSubmit = (e) => {
+		e.preventDefault();
+		getProductsByMinPrice(minPrice);
+	};
 
-  const onMaxSubmit = (e) => {
-    e.preventDefault();
-    getProductsByMaxPrice(maxPrice);
-  };
-  return (
-    <div>
-      <h4>Filter by</h4>
+	const onMaxSubmit = (e) => {
+		e.preventDefault();
+		getProductsByMaxPrice(maxPrice);
+	};
+	return (
+		<div>
+			<h4>Filter by</h4>
 
-      <form
-        className={classes.root}
-        noValidate
-        autoComplete="off"
-        onSubmit={onSubmit}
-      >
-        <TextField
-          id="standard-basic"
-          label="keyword"
-          name="keyword"
-          onChange={handleKeyword}
-        />
-      </form>
+			<form
+				className={classes.root}
+				noValidate
+				autoComplete="off"
+				onSubmit={onSubmit}
+			>
+				<TextField
+					id="standard-basic"
+					label="keyword"
+					name="keyword"
+					onChange={handleKeyword}
+				/>
+			</form>
 
-      <form onSubmit={onMinSubmit} className={classes.root} autoComplete="off">
-        <TextField
-          id="standard-basic"
-          label="min-price"
-          name="min_price"
-          onChange={handleMinPrice}
-        />
-      </form>
-      <form onSubmit={onMaxSubmit} className={classes.root} autoComplete="off">
-        <TextField
-          id="standard-basic"
-          label="max-price"
-          name="max_price"
-          onChange={handleMaxPrice}
-        />
-      </form>
-    </div>
-  );
+			<form onSubmit={onMinSubmit} className={classes.root} autoComplete="off">
+				<TextField
+					id="standard-basic"
+					label="min-price"
+					name="min_price"
+					onChange={handleMinPrice}
+				/>
+			</form>
+			<form onSubmit={onMaxSubmit} className={classes.root} autoComplete="off">
+				<TextField
+					id="standard-basic"
+					label="max-price"
+					name="max_price"
+					onChange={handleMaxPrice}
+				/>
+			</form>
+		</div>
+	);
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    getProductsByMinPrice: (price) => {
-      dispatch(getProductsByMinPrice(price));
-    },
-    getProductsByMaxPrice: (price) => {
-      dispatch(getProductsByMaxPrice(price));
-    },
-    getProductsByKeyword: (keyword) => {
-      dispatch(getProductsByKeyword(keyword));
-    },
-  };
+	return {
+		getProductsByMinPrice: (price) => {
+			dispatch(getProductsByMinPrice(price));
+		},
+		getProductsByMaxPrice: (price) => {
+			dispatch(getProductsByMaxPrice(price));
+		},
+		getProductsByKeyword: (keyword) => {
+			dispatch(getProductsByKeyword(keyword));
+		}
+	};
 };
 
 export default connect(null, mapDispatchToProps)(Filters);
--- src/components/shared/SideBar.js
+++ src/components/shared/SideBar.js
@@ -1,95 +1,86 @@
-// import { useBooleanKnob } from "@stardust-ui/docs-components";
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { getProductsByCategory } from "../../actions/products";
-import { connect } from "react-redux";
+import React, {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
+import {getProductsByCategory} from '../../redux/products/products.actions'
+import {connect} from "react-redux";
 import "./shared.css";
 import Filters from "./Filters";
 
-const SideBar = ({ getProductsByCategory, user, isLogged }) => {
-  const [type, setType] = useState("");
+const SideBar = ({getProductsByCategory, user, isLogged}) => {
+	const [type, setType] = useState("");
 
-  useEffect(() => {
-    getProductsByCategory(type);
-  }, [type]);
+	useEffect(() => {
+		getProductsByCategory(type);
+	}, [type]);
 
-  const handleChange = (e) => {
-    setType(e.target.name);
-  };
-  return (
-    <div className="sidebar">
-      <button className="sidebar-option" name="Stickers" onClick={handleChange}>
-        Stickers
-      </button>
-      <button className="sidebar-option" name="Shirts" onClick={handleChange}>
-        Shirts
-      </button>
-      <button className="sidebar-option" name="Dress" onClick={handleChange}>
-        Dress
-      </button>
-      <button className="sidebar-option" name="Buttons" onClick={handleChange}>
-        Buttons
-      </button>
-      <button
-        className="sidebar-option"
-        name="Posters/Prints"
-        onClick={handleChange}
-      >
-        Posters/Prints
-      </button>
-      <button
-        className="sidebar-option"
-        name="Custome_others"
-        onClick={handleChange}
-      >
-        Custome others
-      </button>
-      <button className="sidebar-option" name="Pants" onClick={handleChange}>
-        Pants
-      </button>
-      <button
-        className="sidebar-option"
-        name="Outfit_dolls"
-        onClick={handleChange}
-      >
-        Outfit dolls
-      </button>
-      <button
-        className="sidebar-option"
-        name="Hair_bows"
-        onClick={handleChange}
-      >
-        Hair bows
-      </button>
-      <button className="sidebar-option" name="Pins" onClick={handleChange}>
-        Pins
-      </button>
-      {isLogged && user.is_admin && (
-        <>
-          <hr />
-          <Link to="/new-product">
-            <button className="sidebar-option">Create product</button>
-          </Link>
-        </>
-      )}
-      <hr />
-      <Filters />
-    </div>
-  );
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getProductsByCategory: (type) => {
-      dispatch(getProductsByCategory(type));
-    },
-  };
+	const handleChange = (e) => {
+		setType(e.target.name);
+	};
+	return (
+		<div className="sidebar">
+			<button className="sidebar-option" name="Stickers" onClick={handleChange}>
+				Stickers
+			</button>
+			<button className="sidebar-option" name="Shirts" onClick={handleChange}>
+				Shirts
+			</button>
+			<button className="sidebar-option" name="Dress" onClick={handleChange}>
+				Dress
+			</button>
+			<button className="sidebar-option" name="Buttons" onClick={handleChange}>
+				Buttons
+			</button>
+			<button
+				className="sidebar-option"
+				name="Posters/Prints"
+				onClick={handleChange}
+			>
+				Posters/Prints
+			</button>
+			<button
+				className="sidebar-option"
+				name="Custome_others"
+				onClick={handleChange}
+			>
+				Custome others
+			</button>
+			<button className="sidebar-option" name="Pants" onClick={handleChange}>
+				Pants
+			</button>
+			<button
+				className="sidebar-option"
+				name="Outfit_dolls"
+				onClick={handleChange}
+			>
+				Outfit dolls
+			</button>
+			<button
+				className="sidebar-option"
+				name="Hair_bows"
+				onClick={handleChange}
+			>
+				Hair bows
+			</button>
+			<button className="sidebar-option" name="Pins" onClick={handleChange}>
+				Pins
+			</button>
+			{isLogged && user.is_admin && (
+				<>
+					<hr/>
+					<Link to="/new-product">
+						<button className="sidebar-option">Create product</button>
+					</Link>
+				</>
+			)}
+			<hr/>
+			<Filters/>
+		</div>
+	);
 };
 
 const mapStateToProps = (state) => {
-  return {
-    user: state.user.user,
-    isLogged: state.user.isLoggedIn,
-  };
+	return {
+		user: state.user.user,
+		isLogged: state.user.isLoggedIn,
+	};
 };
-export default connect(mapStateToProps, mapDispatchToProps)(SideBar);
+export default connect(mapStateToProps, {getProductsByCategory})(SideBar);
